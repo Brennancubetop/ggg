d3ba6e997d739ddde4d73cc7b9d143e1ef9c53d6
Brennancubetop <brennancubetop@gmail.com>
2025-02-23 13:41:54 +0100

    Update game.py

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ def guess_the_number():
     number = random.randint(1, 100)
     attempts = 0
 
-    print("Я загадал число от 1 до 100. Попробуйте угадать!")
+    print("Я загадал число от 1 до 200. Попробуйте угадать!")
 
     while True:
         try:
